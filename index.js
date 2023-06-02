d883dd3da85f96798d0071fd1b5ff08b45c21c4e
bharath1098 <120422830+bharath1098@users.noreply.github.com>
2023-06-02 12:57:47 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+document.getElementById("login").addEventListener("click", () => {
+    window.location.href = "../login/login.html"
+})
+
+document.getElementById("signup").addEventListener("click", () => {
+    window.location.href = "../signup/signup.html"
+})
+
+const currentUser = localStorage.getItem("currentUser");
+
+if (currentUser !== null) {
+    window.location.href = "../shop/shop.html";
+}
+console.log("object")
